80cfb086920e1f36998ab50f229d74a4c45faa97
PabloJ4 <008pablo008@gmail.com>
2024-04-29 21:00:44 -0300

    realizado modificações no css de ecperiencia

--- validate.js
+++ validate.js
@@ -1,1 +1,10 @@
 //Seu JavaScript de validação aqui
+var email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("spero um e-mail, querida!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
